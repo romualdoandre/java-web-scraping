71c7e90d13e2b09123c2401e03fdc05148bd472e
romualdoandre <romualdoandre@gmail.com>
2020-08-31 17:17:17 -0300

    indicadores principais

--- src/main/java/br/com/programadorfeirense/factory/StockFactory.java
+++ src/main/java/br/com/programadorfeirense/factory/StockFactory.java
@@ -5,23 +5,65 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import br.com.programadorfeirense.model.Stock;
 
-/** Classe para extrair stock de um documento*/
+/** Classe para extrair stock de um documento */
 public class StockFactory {
 	private HtmlPage page;
+
 	public StockFactory(HtmlPage page) {
-		this.page=page;
+		this.page = page;
 	}
-	
+
 	public Stock buildStock(String ticker) {
-		Stock stock=new Stock(ticker);
+		Stock stock = new Stock(ticker);
 		DomNode label = page.querySelector(
-				"body > div.center > div.conteudo.clearfix > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(3) > span.txt");
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(3) > span.txt");
 		DomNode value = page.querySelector(
-				"body > div.center > div.conteudo.clearfix > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(4) > span");
-		
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(4) > span.txt");
+
+		// P/L
 		System.out.println(label.getTextContent());
 		System.out.println(value.getTextContent());
-		stock.setPl(Float.valueOf( value.getTextContent().replace(',', '.')));
+		try {
+			stock.setPl(Float.valueOf(value.getTextContent().replace(',', '.').trim()));
+		} catch (NumberFormatException e) {
+			stock.setPl(0);
+		}
+		// DY
+		label = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(9) > td:nth-child(3) > span.txt");
+		value = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(9) > td:nth-child(4) > span.txt");
+		System.out.println(label.getTextContent());
+		System.out.println(value.getTextContent());
+		try {
+			stock.setDy(Float.valueOf(value.getTextContent().replace(',', '.').replace('%', ' ').trim()));
+		} catch (NumberFormatException e) {
+			stock.setDy(0);
+		}
+		// P/VPA
+		label = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(3) > span.txt");
+		value = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(3) > td:nth-child(4) > span.txt");
+		System.out.println(label.getTextContent());
+		System.out.println(value.getTextContent());
+		try {
+			stock.setPvpa(Float.valueOf(value.getTextContent().replace(',', '.').trim()));
+		} catch (NumberFormatException e) {
+			stock.setPvpa(0);
+		}
+		// EV/EBITDA
+		label = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(10) > td:nth-child(3) > span.txt");
+		value = page.querySelector(
+				"body > div.center > div.conteudo.clearfix > table:nth-child(4) > tbody > tr:nth-child(10) > td:nth-child(4) > span.txt");
+		System.out.println(label.getTextContent());
+		System.out.println(value.getTextContent());
+		try {
+			stock.setEvebitda(Float.valueOf(value.getTextContent().replace(',', '.').trim()));
+		} catch (NumberFormatException e) {
+			stock.setEvebitda(0);
+		}
 		System.out.println(stock);
 		return stock;
 	}
